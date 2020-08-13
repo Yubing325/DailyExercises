0c49b00b4ec14c59e5af7377d5c0a0e2900b1b54
Yubing325 <35515298+Yubing325@users.noreply.github.com>
2020-08-12 21:40:18 -0500

    js sort

---
+++ Javascript/sort/index.js
@@ -0,0 +1,26 @@
+nums = [0,10,4,9,123,54,1];
+
+var a = nums.sort();
+
+console.log(a);
+
+let some = new Array(10).fill(null);
+
+console.log(some);
+
+some = some.map( 
+        () => (Math.floor(Math.random()*200)+1)
+    );
+console.log(some);
+
+some.sort();
+
+console.log(some);
+
+some.sort((a,b)=> a - b);
+
+console.log(some);
+
+some.sort((a,b)=> b - a);
+
+console.log(some);
