36f1e03f0f1637212d168c58b3afc813d24499ff
Yubing325 <35515298+Yubing325@users.noreply.github.com>
2020-08-12 21:45:05 -0500

    get unique of js

---
+++ Javascript/getUnique/index.js
@@ -0,0 +1,9 @@
+let uniqueArray = [...new Set([1, 2, 3, 3,3,"school","school",'ball',false,false,true,true])];
+
+console.log(uniqueArray)
+
+nums = [1,2,3,3,6,3,8];
+
+var some = [...new Set(nums)];
+
+console.log(some);
