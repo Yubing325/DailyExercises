6a8f8fc2aee14cf621b61a6215954b191edba338
Yubing325 <35515298+Yubing325@users.noreply.github.com>
2020-08-15 16:23:03 -0500

    function ctor of js

---
+++ Javascript/function ctors/index.js
@@ -0,0 +1,20 @@
+//Function Constructor
+// factory function:
+var john = {
+    name: 'John',
+    yearOfBirth: 1990,
+    job: 'teacher'
+};
+
+
+// function constructor:
+var Person = function(name, yearOfBirth, job){
+    this.name = name;
+    this.yearOfBirth = yearOfBirth;
+    this.job = job;    
+}
+
+var Bob = new Person('Bob', 1992, 'sales');
+
+
+// https://stackoverflow.com/questions/8698726/constructor-function-vs-factory-functions#:~:text=A%20constructor%20returns%20an%20instance,has%20a%20large%20setup%20process.
