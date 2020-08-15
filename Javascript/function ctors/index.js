37774eb0ec5c1c1d560fb9740e56e1f545823855
Yubing325 <35515298+Yubing325@users.noreply.github.com>
2020-08-15 16:33:57 -0500

    add calculateAge

--- Javascript/function ctors/index.js
+++ Javascript/function ctors/index.js
@@ -11,10 +11,15 @@ var john = {
 var Person = function(name, yearOfBirth, job){
     this.name = name;
     this.yearOfBirth = yearOfBirth;
-    this.job = job;    
+    this.job = job;
+    this.calculateAge = function() {
+        let thisYear = new Date().getFullYear();
+        console.log(thisYear - yearOfBirth)
+    }    
 }
 
 var Bob = new Person('Bob', 1992, 'sales');
 
+Bob.calculateAge();
 
 // https://stackoverflow.com/questions/8698726/constructor-function-vs-factory-functions#:~:text=A%20constructor%20returns%20an%20instance,has%20a%20large%20setup%20process.
