b0bea2d588fa0489c6c764fb696c22301d17fac8
Yubing325 <35515298+Yubing325@users.noreply.github.com>
2020-08-17 19:10:13 -0500

    init the quiz

---
+++ Javascript/quiz/find-largest-equal-to-rest.js
@@ -0,0 +1,18 @@
+// given an array, find if the largest number equal the sum of rest elements.
+
+var array = [1,3,5,6,15]; //should output true
+
+
+function ArraySum(array){
+    //find the largest number in the array save to a temp variable e.g: t
+        // order the array 
+        array = array.sort((a,b)=>a-b);
+        console.log(array);
+//remove it from the array
+
+//calculate the sum of the rest, save the sum to tem variable t2
+
+// check if t==t2
+
+
+}
