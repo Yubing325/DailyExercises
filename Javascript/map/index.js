f2da4418d1b0537adedd35e0cb6d251425a3673b
Yubing325 <35515298+Yubing325@users.noreply.github.com>
2020-08-19 22:00:46 -0500

    javascrip map && map vs forEach

---
+++ Javascript/map/index.js
@@ -0,0 +1,26 @@
+//The map() method creates a new array populated with the results of calling a provided function on every element in the calling array.
+
+const array1 = [1, 4, 9, 16];
+
+// pass a function to map
+const map1 = array1.map(x => x * 2);
+
+console.log(map1);
+// expected output: Array [2, 8, 18, 32]
+console.log(array1); // 1,4,9,16
+
+//<============ Map vs Foreach:===========
+
+let arry = [1,2,3,4,5];
+
+console.log(arry);
+
+arry.forEach((x,i)=>{
+    arry[i]=x *2;
+});
+
+console.log(arry); //arry got mututated by forEach
+
+console.log(arry.map(t=>t*2));
+
+console.log(arry);
