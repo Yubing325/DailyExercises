86aac46227aa72ed325e276eccc63cb36a1389b8
Yubing325 <35515298+Yubing325@users.noreply.github.com>
2020-09-08 19:49:40 -0500

    add source

--- Javascript/object-class/class.ts
+++ Javascript/object-class/class.ts
@@ -10,3 +10,5 @@ const pizza = PizzaMaker.create({
 })
 
 console.log(pizza);
+
+//https://ultimatecourses.com/blog/classes-vs-interfaces-in-typescript#:~:text=Learn%20TypeScript%20the%20right%20way!&text=A%20class%20is%20a%20blueprint,implementation%20nor%20initialisation%20for%20them.
